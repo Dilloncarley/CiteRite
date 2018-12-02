541cbe822a1ddbf50baadbad808ed575cedf0b68
David Fulton <dlf95@its.msstate.edu>
2018-12-02 15:43:49 -0600

    Problems with the install information.
    The system is not being installed at the root of the public location.  It is a subdirectory for each group.  The directions are for it being at the root.
    Please update.

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
-<?php 
-    require '../CiteRite/vendor/autoload.php';
-    require '../CiteRite/config/config.php';
-    
+<?php
+    require '../group4/vendor/autoload.php';
+    require '../group4/config/config.php';
+
     if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) { return false; }
-    
+
     //config app with settings
     $app = new \Slim\Slim();
-    
+
     $app->config($config_array);
     $settingValue = $app->config('templates.path');
     $loader = new Twig_Loader_Filesystem($settingValue);
@@ -15,11 +15,11 @@
     ));
     //set timezone
     date_default_timezone_set("America/Chicago");
-    
+
     //database set up PDO class
     $db = new PDO('mysql:host='.$app->config('dbHost').';dbname='.$app->config('dbName').'', $app->config('dbUser'), $app->config('dbPass'));
-    
-    
+
+
     // App routes
     require '../CiteRite/src/routes.php';
     // Run app
